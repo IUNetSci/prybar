21d6aaea76a9f8ba4966dc9408f6de66441eb35e
Clayton Davis <clayadavis@gmail.com>
2016-08-02 09:40:20 -0400

    Add buttons for each example action

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,19 @@
 prybar = new Prybar('#svg-example');
 
-var exportBtn = document.getElementById('btn-export');
-exportBtn.addEventListener('click', function (e) {
-      var canvas = prybar.toCanvas(),
-          dataURL = canvas.toDataURL('image/png');
-      window.open(dataURL);
-});
+document.getElementById('btn-to-canvas')
+  .addEventListener('click', function (e) {
+    var canvas = prybar.toCanvas();
+    document.body.appendChild(canvas);
+  });
+
+document.getElementById('btn-export-png')
+  .addEventListener('click', function (e) {
+    var canvas = prybar.toCanvas(),
+        dataURL = canvas.toDataURL('image/png');
+    window.open(dataURL);
+  });
+
+document.getElementById('btn-export-svg')
+  .addEventListener('click', function (e) {
+    prybar.exportSvg('exported_plot.svg');
+  });
